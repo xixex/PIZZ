ae88f251991086832958e430fd5eae7aac10f636
Mikhail <39026742+xixex@users.noreply.github.com>
2019-04-18 20:52:34 +0300

    fast anim

--- src/components/pizza.js
+++ src/components/pizza.js
@@ -3,10 +3,11 @@ const createPizza = pizzaPieces => {
   const pizza = document.createElement("div");
   pizza.className = "pizza";
 
-  for (let i = 0, deg = 0; i < pizzaPieces; i++, deg += sliceDeg) {
+  for (let deg = 0; deg < 360; deg += sliceDeg) {
     const piece = document.createElement("div");
     piece.className = "piece";
     piece.style.transform = `rotate(${deg}deg)`;
+    piece.style.animationDelay = `${0.5 + deg / 700}s`;
     pizza.appendChild(piece);
   }
   return pizza;
